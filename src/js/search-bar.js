09e7d318ac04e60ce155f1e539a57c0973ff6f05
lihongxun945 <lihongxun945@gmail.com>
2016-03-03 15:37:01 +0800

    修复搜索栏 focus 类拼写错误

--- src/js/search-bar.js
+++ src/js/search-bar.js
@@ -3,10 +3,10 @@
   "use strict";
 
   $(document).on("click", ".weui_search_bar label", function(e) {
-    $(e.target).parents(".weui_search_bar").addClass("weui_search_focus");
+    $(e.target).parents(".weui_search_bar").addClass("weui_search_focusing");
   }) 
   .on("blur", ".weui_search_input", function(e) {
-    $(e.target).parents(".weui_search_bar").removeClass("weui_search_focus");
+    $(e.target).parents(".weui_search_bar").removeClass("weui_search_focusing");
   })
 
 }($);
