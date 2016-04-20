e99fa026b207e9344779e7ae5e1617175ccf9632
lihongxun945 <lihongxun945@gmail.com>
2016-04-20 15:01:46 +0800

    add compressed js and css

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,9 @@ var connect = require("gulp-connect");
 var less = require("gulp-less");
 var autoprefixer = require('gulp-autoprefixer');
 var ejs = require("gulp-ejs");
+var uglify = require('gulp-uglify');
+var ext_replace = require('gulp-ext-replace');
+var cssmin = require('gulp-cssmin');
 
 var pkg = require("./package.json");
 
@@ -15,20 +18,29 @@ var banner =
 * http://lihongxun945.github.io/jquery-weui/\n \
 */\n";
 
-gulp.task('js', function() {
+gulp.task('js', function(cb) {
+
+  count = 0;
+  var end = function(){
+    count ++;
+    if(count >= 3) cb();
+  };
+
   gulp.src([
     './src/js/city-data.js',
     './src/js/city-picker.js'
   ])
     .pipe(concat({ path: 'city-picker.js'}))
-    .pipe(gulp.dest('./dist/js/'));
+    .pipe(gulp.dest('./dist/js/'))
+    .on("end", end);
 
   gulp.src([
     './src/js/swiper.jquery.js',
     './src/js/swiper-wrap.js'
   ])
     .pipe(concat({ path: 'swiper.js'}))
-    .pipe(gulp.dest('./dist/js/'));
+    .pipe(gulp.dest('./dist/js/'))
+    .on("end", end);
 
   gulp.src([
     './src/js/jquery-extend.js',
@@ -50,7 +62,19 @@ gulp.task('js', function() {
   ])
     .pipe(concat({ path: 'jquery-weui.js'}))
     .pipe(header(banner))
-    .pipe(gulp.dest('./dist/js/'));
+    .pipe(gulp.dest('./dist/js/'))
+    .on("end", end);
+
+  
+});
+
+gulp.task('uglify', ["js"], function() {
+  return gulp.src(['./dist/js/*.js', '!./dist/js/*.min.js'])
+    .pipe(uglify({
+      preserveComments: "license"
+    }))
+    .pipe(ext_replace('.min.js'))
+    .pipe(gulp.dest('./dist/js'));
 });
 
 gulp.task('less', function () {
@@ -59,6 +83,13 @@ gulp.task('less', function () {
   .pipe(autoprefixer())
   .pipe(header(banner))
   .pipe(gulp.dest('./dist/css/'));
+});
+
+gulp.task('cssmin', ["less"], function () {
+  gulp.src(['./dist/css/*.css', '!./dist/css/*.min.css'])
+    .pipe(cssmin())
+    .pipe(ext_replace('.min.css'))
+    .pipe(gulp.dest('./dist/css/'));
 });
 
 gulp.task('ejs', function () {
@@ -88,4 +119,4 @@ gulp.task('watch', function () {
 gulp.task('server', function () {
   connect.server();
 });
-gulp.task("default", ['js', 'less', 'copy', 'ejs']);
+gulp.task("default", ['uglify', 'cssmin', 'copy', 'ejs']);
