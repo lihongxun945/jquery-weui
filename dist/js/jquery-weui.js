60f4598dab017cc78bdbe0137f3e2f68f65dda63
lihongxun945 <lihongxun945@gmail.com>
2016-05-09 14:20:58 +0800

    update photos

--- dist/js/jquery-weui.js
+++ dist/js/jquery-weui.js
@@ -3273,6 +3273,7 @@ Device/OS Detection
 
   var PhotoBrowser = function(config) {
     this.initConfig(config);
+    this.index = 0;
   }
 
   PhotoBrowser.prototype.initConfig = function(config) {
@@ -3311,25 +3312,30 @@ Device/OS Detection
     this.modal.addClass("weui-photo-browser-modal-visible");
     swiperContainer.transitionEnd($.proxy(function() {
       swiperContainer.swiper({
-        onSlideChangeEnd: this.onSlideChangeEnd
+        onSlideChangeEnd: $.proxy(this.onSlideChangeEnd, this)
       });
       this.onSlideChangeEnd(swiperContainer.data("swiper"));
     }, this));
 
     swiperContainer.addClass("swiper-container-visible");
+
+    this._open = true;
+
+    if(this.config.onOpen) this.config.onOpen.call(this);
   }
 
   PhotoBrowser.prototype.close = function() {
     this.swiperContainer.transitionEnd($.proxy(function() {
       this.modal.hide();
       this._open = false;
+      if(this.config.onClose) this.config.onClose.call(this);
     }, this));
     this.swiperContainer.removeClass("swiper-container-visible");
     this.modal.removeClass("weui-photo-browser-modal-visible");
   }
 
   PhotoBrowser.prototype.onSlideChangeEnd = function(swiper) {
-    var index = swiper.snapIndex;
+    var index = this.index = swiper.snapIndex;
     var next = swiper.container.find(".caption-item-"+index);
 
     if(next.hasClass("active")) return;
@@ -3344,6 +3350,9 @@ Device/OS Detection
     if(!current[0]) {
       next.show().addClass('active');
     }
+
+    if(this.config.onSlideChange) this.config.onSlideChange.call(this, index);
+
   }
 
   defaults = PhotoBrowser.prototype.defaults = {
