2fac9141b05c9753bb076f3c343db4c998457215
hongxun.lhx <hongxun.lhx@alibaba-inc.com>
2016-07-17 15:33:30 +0800

    update modal args

--- src/js/modal.js
+++ src/js/modal.js
@@ -3,7 +3,7 @@
 
   var defaults;
   
-  $.modal = function(params) {
+  $.modal = function(params, onOpen) {
     params = $.extend({}, defaults, params);
 
 
@@ -19,7 +19,7 @@
                 '<div class="weui_dialog_ft">' + buttonsHtml + '</div>' +
               '</div>';
     
-    var dialog = $.openModal(tpl);
+    var dialog = $.openModal(tpl, onOpen);
 
     dialog.find(".weui_btn_dialog").each(function(i, e) {
       var el = $(e);
@@ -32,17 +32,26 @@
         }
       });
     });
+
+    return dialog;
   };
 
-  $.openModal = function(tpl) {
+  $.openModal = function(tpl, onOpen) {
     var mask = $("<div class='weui_mask'></div>").appendTo(document.body);
     mask.show();
 
     var dialog = $(tpl).appendTo(document.body);
-    
+ 
+    if (onOpen) {
+      dialog.transitionEnd(function () {
+        onOpen.call(dialog);
+      });
+    }   
+
     dialog.show();
     mask.addClass("weui_mask_visible");
     dialog.addClass("weui_dialog_visible");
+
 
     return dialog;
   }
@@ -56,69 +65,109 @@
     });
   };
 
-  $.alert = function(text, title, callback) {
-    if (typeof title === 'function') {
-      callback = arguments[1];
-      title = undefined;
+  $.alert = function(text, title, onOK) {
+    var config;
+    if (typeof text === 'object') {
+      config = text;
+    } else {
+      if (typeof title === 'function') {
+        onOK = arguments[1];
+        title = undefined;
+      }
+
+      config = {
+        text: text,
+        title: title,
+        onOK: onOK
+      }
     }
     return $.modal({
-      text: text,
-      title: title,
+      text: config.text,
+      title: config.title,
       buttons: [{
         text: defaults.buttonOK,
         className: "primary",
-        onClick: callback
+        onClick: config.onOK
       }]
     });
   }
 
-  $.confirm = function(text, title, callbackOK, callbackCancel) {
-    if (typeof title === 'function') {
-      callbackCancel = arguments[2];
-      callbackOK = arguments[1];
-      title = undefined;
+  $.confirm = function(text, title, onOK, onCancel) {
+    var config;
+    if (typeof text === 'object') {
+      config = text
+    } else {
+      if (typeof title === 'function') {
+        onCancel = arguments[2];
+        onOK = arguments[1];
+        title = undefined;
+      }
+
+      config = {
+        text: text,
+        title: title,
+        onOK: onOK,
+        onCancel: onCancel
+      }
     }
     return $.modal({
-      text: text,
-      title: title,
+      text: config.text,
+      title: config.title,
       buttons: [
       {
         text: defaults.buttonCancel,
         className: "default",
-        onClick: callbackCancel
+        onClick: config.onCancel
       },
       {
         text: defaults.buttonOK,
         className: "primary",
-        onClick: callbackOK
+        onClick: config.onOK
       }]
     });
   };
 
-  $.prompt = function(text, title, callbackOK, callbackCancel) {
-    if (typeof title === 'function') {
-      callbackCancel = arguments[2];
-      callbackOK = arguments[1];
-      title = undefined;
+  //如果参数过多，建议通过 config 对象进行配置，而不是传入多个参数。
+  $.prompt = function(text, title, onOK, onCancel, input) {
+    var config;
+    if (typeof text === 'object') {
+      config = text;
+    } else {
+      if (typeof title === 'function') {
+        onCancel = arguments[2];
+        onOK = arguments[1];
+        title = undefined;
+      }
+      config = {
+        text: text,
+        title: title,
+        input: input,
+        onOK: onOK,
+        onCancel: onCancel
+      }
     }
 
-    return $.modal({
-      text: "<p class='weui-prompt-text'>"+(text || "")+"</p><input type='text' class='weui_input weui-prompt-input' id='weui-prompt-input'/>",
-      title: title,
+    var modal = $.modal({
+      text: '<p class="weui-prompt-text">'+(config.text || '')+'</p><input type="text" class="weui_input weui-prompt-input" id="weui-prompt-input" value="' + (config.input || '') + '" />',
+      title: config.title,
       buttons: [
       {
         text: defaults.buttonCancel,
         className: "default",
-        onClick: callbackCancel
+        onClick: config.onCancel
       },
       {
         text: defaults.buttonOK,
         className: "primary",
         onClick: function() {
-          callbackOK && callbackOK($("#weui-prompt-input").val());
+          config.onOK && config.onOK($("#weui-prompt-input").val());
         }
       }]
+    }, function () {
+      this.find('.weui-prompt-input').focus()[0].select();
     });
+
+    return modal;
   };
 
   defaults = $.modal.prototype.defaults = {
