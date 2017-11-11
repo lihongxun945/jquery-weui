a73343c9f3c082938691a08982f094836e60c57b
lihongxun945 <lihongxun945@gmail.com>
2017-11-11 13:56:43 +0800

    build v1.1.0

--- dist/js/jquery-weui.js
+++ dist/js/jquery-weui.js
@@ -1,5 +1,5 @@
 /** 
-* jQuery WeUI V1.0.1 
+* jQuery WeUI V1.1.0 
 * By 言川
 * http://lihongxun945.github.io/jquery-weui/
  */
@@ -3485,8 +3485,8 @@ if (typeof define === 'function' && define.amd) {
     var tpl = '<div class="weui-toast ' + className + '">' + html + '</div>';
     var dialog = $(tpl).appendTo(document.body);
 
-    dialog.show();
     dialog.addClass("weui-toast--visible");
+    dialog.show();
   };
 
   var hide = function(callback) {
@@ -3558,7 +3558,7 @@ if (typeof define === 'function' && define.amd) {
     var titleHtml = "";
     
     if (params.title) {
-      titleHtml = '<div class="weui-actionsheet__title">' + params.title + '</div>';
+      titleHtml = '<div class="weui-actionsheet__title"><p class="weui-actionsheet__title-text">' + params.title + '</p></div>';
     }
 
     var tpl = '<div class="weui-actionsheet " id="weui-actionsheet">'+
@@ -6273,4 +6273,103 @@ Device/OS Detection
       else $this.data('slider', new Slider(this, arg))
     })
   }
+}($);
+
+/* ===============================================================================
+************   Swipeout ************
+=============================================================================== */
+/* global $:true */
+
++function ($) {
+  "use strict";
+
+  var cache = [];
+
+  var Swipeout = function(el) {
+    this.container = $(el);
+    this.mover = this.container.find('>.weui-cell__bd')
+    this.limit = this.container.find('>.weui-cell__ft').width() || 68;
+    this.attachEvents();
+    cache.push(this)
+  }
+
+  Swipeout.prototype.touchStart = function(e) {
+    var p = $.getTouchPosition(e);
+    this.start = false;
+    this.container.addClass("touching");
+    this.start = p;
+    this.startX = 0;
+    this.startTime = + new Date;
+    var transform =  this.mover.css('transform').match(/-?[\d\.]+/g)
+    if (transform && transform.length) this.startX = parseInt(transform[4])
+    this.diffX = this.diffY = 0;
+    //close others
+    var self = this
+    cache.forEach(function (s) {
+      if (s !== self) s.close()
+    })
+  };
+
+  Swipeout.prototype.touchMove= function(e) {
+    if(!this.start) return false;
+    var p = $.getTouchPosition(e);
+    this.diffX = p.x - this.start.x;
+    e.preventDefault();
+    e.stopPropagation();
+    var x = this.diffX + this.startX
+    if (x > 0) x = 0;
+    if (Math.abs(x) > this.limit) x = - (Math.pow(-(x+this.limit), .7) + this.limit)
+    this.mover.css("transform", "translate3d("+x+"px, 0, 0)");
+  };
+  Swipeout.prototype.touchEnd = function() {
+    this.start = false;
+    this.container.removeClass("touching");
+    var x = this.diffX + this.startX
+    var t = new Date - this.startTime;
+    if (this.diffX < -5 && t < 200) { // 向左快速滑动，则打开
+      this.open()
+    } else if (this.diffX >= 0 && t < 200) { // 向右快速滑动，或者单击,则关闭
+      this.close()
+    } else if (x > 0 || -x <= this.limit / 2) {
+      this.close()
+    } else {
+      this.open()
+    }
+  };
+
+
+  Swipeout.prototype.close = function() {
+    this.mover.css("transform", "translate3d(0, 0, 0)");
+    this.container.trigger('swipeout-close');
+  }
+
+  Swipeout.prototype.open = function() {
+    this.mover.css("transform", "translate3d(" + (-this.limit) + "px, 0, 0)");
+    this.container.trigger('swipeout-open');
+  }
+
+  Swipeout.prototype.attachEvents = function() {
+    var el = this.mover;
+    el.on($.touchEvents.start, $.proxy(this.touchStart, this));
+    el.on($.touchEvents.move, $.proxy(this.touchMove, this));
+    el.on($.touchEvents.end, $.proxy(this.touchEnd, this));
+  };
+
+  var swipeout = function(el) {
+    return new Swipeout(el);
+  };
+
+  $.fn.swipeout = function (arg) {
+    return this.each(function() {
+      var $this = $(this)
+      var s = $this.data('swipeout') || swipeout(this);
+      $this.data('swipeout', s);
+
+      if (typeof arg === typeof 'a') {
+        s[arg]()
+      }
+    });
+  }
+
+  $('.weui-cell_swiped').swipeout() // auto init
 }($);
